278de0b162a20013aa3c9f97e948d40b5b27187c
Kevin Brodsky <kevin.brodsky@arm.com>
2016-11-23 14:01:10 +0000

    FROMLIST: [PATCH v2 1/3] arm64: compat: Split the sigreturn trampolines and kuser helpers (C sources)
    (cherry picked from url http://lkml.iu.edu/hypermail/linux/kernel/1709.1/01901.html)
    
    AArch32 processes are currently installed a special [vectors] page that
    contains the sigreturn trampolines and the kuser helpers, at the fixed
    address mandated by the kuser helpers ABI.
    
    Having both functionalities in the same page has become problematic,
    because:
    
    * It makes it impossible to disable the kuser helpers (the sigreturn
      trampolines cannot be removed), which is possible on arm.
    
    * A future 32-bit vDSO would provide the sigreturn trampolines itself,
      making those in [vectors] redundant.
    
    This patch addresses the problem by moving the sigreturn trampolines to
    a separate [sigpage] page, mirroring [sigpage] on arm.
    
    Even though [vectors] has always been a misnomer on arm64/compat, as
    there is no AArch32 vector there (and now only the kuser helpers),
    its name has been left unchanged, for compatibility with arm (there
    are reports of software relying on [vectors] being there as the last
    mapping in /proc/maps).
    
    mm->context.vdso used to point to the [vectors] page, which is
    unnecessary (as its address is fixed). It now points to the [sigpage]
    page (whose address is randomized like a vDSO).
    
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>
    Signed-off-by: Mark Salyzyn <salyzyn@android.com>
    Bug: 9674955
    Bug: 63737556
    Bug: 20045882
    Change-Id: I52a56ea71d7326df8c784f90eb73b5c324fe9d20
    
    	modified:   arch/arm64/include/asm/signal32.h
    	modified:   arch/arm64/kernel/signal32.c
    	modified:   arch/arm64/kernel/vdso.c

---
+++ build-eas.sh
@@ -0,0 +1,36 @@
+##################
+#   Phoenix-HMP  #
+##################
+# Clean old out folder
+rm -rf '/home/kazu/android_kernel_xiaomi_lavender/out'
+# Set defaults
+cd "/home/kazu/android_kernel_xiaomi_lavender"
+wd=$(pwd)
+out=$wd/out
+BUILD="/home/android_kernel_xiaomi_lavender"
+# Set kernel source workspace
+cd $BUILD
+# Export ARCH <arm, arm64, x86, x86_64>
+export ARCH=arm64
+# Export SUBARCH <arm, arm64, x86, x86_64>
+export SUBARCH=arm64
+# Set kernal name
+# export LOCALVERSION=-
+# Export Username
+export KBUILD_BUILD_USER=KazuDante
+# Export Machine name
+export KBUILD_BUILD_HOST=xdadevelopers
+# Compiler String
+CC=/home/kazu/platform_prebuilts_clang_host_linux-x86_clang_5900059/bin/clang
+export KBUILD_COMPILER_STRING="$(${CC} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+# Make and Clean
+make O=$out clean
+make O=$out mrproper
+# Make <defconfig>
+make O=$out ARCH=arm64 lavender-perf_defconfig
+# Build Kernel
+make O=$out ARCH=arm64 \
+CC="/home/kazu/platform_prebuilts_clang_host_linux-x86_clang_5900059/bin/clang" \
+CROSS_COMPILE="/home/kazu/toolchain/toolchain64/bin/aarch64-linux-gnu-" \
+CROSS_COMPILE_ARM32="/home/kazu/toolchain/toolchain32/bin/arm-linux-gnueabi-" \
+-j$(nproc --all) Image.gz-dtb
