7b2e9bbd0b1be524708bccee4922d5479503e30c
Leo Yan <leo.yan@linaro.org>
2017-03-27 15:00:14 +0100

    sched/fair: kick nohz idle balance for misfit task
    If there have misfit task on one CPU, current code does not handle this
    situation for nohz idle balance. As result, we can see the misfit task
    stays run on little core for long time.
    
    So this patch check if the CPU has misfit task or not. If has misfit
    task then kick nohz idle balance so finally can execute active balance.
    
    Change-Id: I117d3b7404296f8de11cb960a87a6b9a54a9f348
    Signed-off-by: Leo Yan <leo.yan at linaro.org>
    [taken from https://lists.linaro.org/pipermail/eas-dev/2016-September/000551.html]
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    
    	modified:   kernel/sched/fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7423,7 +7423,7 @@ static bool yield_to_task_fair(struct rq *rq, struct task_struct *p, bool preemp
  *
  * The adjacency matrix of the resulting graph is given by:
  *
- *             log_2 n     
+ *             log_2 n
  *   A_i,j = \Union     (i % 2^k == 0) && i / 2^(k+1) == j / 2^(k+1)  (6)
  *             k = 0
  *
@@ -7469,7 +7469,7 @@ static bool yield_to_task_fair(struct rq *rq, struct task_struct *p, bool preemp
  *
  * [XXX write more on how we solve this.. _after_ merging pjt's patches that
  *      rewrite all of this once again.]
- */ 
+ */
 
 static unsigned long __read_mostly max_load_balance_interval = HZ/10;
 
@@ -8189,7 +8189,7 @@ void update_group_capacity(struct sched_domain *sd, int cpu)
 		/*
 		 * !SD_OVERLAP domains can assume that child groups
 		 * span the current group.
-		 */ 
+		 */
 
 		group = child->groups;
 		do {
@@ -10020,8 +10020,8 @@ static inline bool nohz_kick_needed(struct rq *rq)
 		return true;
 
 	/* Do idle load balance if there have misfit task */
-	if (energy_aware())
-		return rq->misfit_task;
+	if (energy_aware() && rq->misfit_task)
+		return true;
 
 	rcu_read_lock();
 	sd = rcu_dereference(per_cpu(sd_busy, cpu));
