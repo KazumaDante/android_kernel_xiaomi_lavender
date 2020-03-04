bddffc45c26700fbdd404ed235929843ae70b07b
KazuDante89 <karlemmanueldorvil@gmail.com>
2020-03-04 13:48:12 -0500

    Revert "workqueue: Schedule workers on CPU0 or CPU0/CPU1 by default"
    This reverts commit c1a0a1b4d00759cdbba7746e77d34e465e058d05.
    
    	modified:   kernel/workqueue.c

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1369,9 +1369,6 @@ static void __queue_work(int cpu, struct workqueue_struct *wq,
 	if (unlikely(wq->flags & __WQ_DRAINING) &&
 	    WARN_ON_ONCE(!is_chained_work(wq)))
 		return;
-	if (req_cpu == WORK_CPU_UNBOUND)
-		cpu = 0;
-retry:
 
 	if (req_cpu == WORK_CPU_UNBOUND)
 		cpu = 0;
